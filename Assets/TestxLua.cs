0c941c946287cb2b5119ecf40028be1d168276eb
songqin <songqin@youkia.net>
2018-11-21 16:25:50 +0800

    Initial commit

---
+++ Assets/TestxLua.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using XLua.LuaDLL;
+
+public class TestxLua : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+       LuaEnv luaEnv = new LuaEnv();
+
+
+	    luaEnv.DoString(@"lua.hotfix()");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
